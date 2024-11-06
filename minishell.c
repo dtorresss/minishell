848e5ecf1ae22dce464732a50ce3965311a24b86
dtorresss <diegotorresprofesional@gmail.com>
2024-11-06 20:20:28 +0100

    Starting pipes

--- minishell.c
+++ minishell.c
@@ -25,33 +25,34 @@ int main(){
         
         pid = fork();
         if (pid == 0){
-            for (i=0; i<line->ncommands; i++) {
-                if (line->redirect_input != NULL) {
-                    int fd_in = open(line->redirect_input, O_RDONLY);
-                    if (fd_in < 0) {
-                        printf("Error al abrir el archivo de entrada\n");
-                        exit(1);
-                    }
-                    dup2(fd_in, STDIN_FILENO);
-                    close(fd_in);
-		        }  
-		        if (line->redirect_output != NULL) {
-                    int fd_out = open(line->redirect_output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-                    if (fd_out < 0) {
-                        printf("Error al abrir el archivo de salida");
-                        exit(1);
-                    }
-                    dup2(fd_out, STDOUT_FILENO);
-                    close(fd_out);
-		        }
-                /*printf("orden %d (%s):\n", i, line->commands[i].filename);
+            printf("EEEE%d", line->ncommands);
+            if (line->redirect_input != NULL) {
+                int fd_in = open(line->redirect_input, O_RDONLY);
+                if (fd_in < 0) {
+                    printf("Error al abrir el archivo de entrada\n");
+                    exit(1);
+                }
+                dup2(fd_in, STDIN_FILENO);
+                close(fd_in);
+		    }  
+		    if (line->redirect_output != NULL) {
+                int fd_out = open(line->redirect_output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+                if (fd_out < 0) {
+                    printf("Error al abrir el archivo de salida");
+                    exit(1);
+                }
+                dup2(fd_out, STDOUT_FILENO);
+                close(fd_out);
+		    }
+            /*for (i=0; i<line->ncommands; i++) {
+                printf("orden %d (%s):\n", i, line->commands[i].filename);
                 for (j=0; j<line->commands[i].argc; j++) {
                     printf("  argumento %d: %s\n", j, line->commands[i].argv[j]);
-                }*/
-                execvp(line->commands->argv[0], line->commands->argv);
-                fprintf(stderr,"Se ha producido un error.\n");
-                exit(1);
-            }
+                }
+		    }*/
+            execvp(line->commands->argv[0], line->commands->argv);
+            fprintf(stderr,"Se ha producido un error.\n");
+            exit(1);
         }
         else {
             wait(NULL);
