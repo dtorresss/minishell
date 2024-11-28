3292b1375e67b23aeb8b5c6886aaf8a9ccd77663
dtorresss <diegotorresprofesional@gmail.com>
2024-11-28 11:56:12 +0100

    El cd se puede hacer ya

--- myshell.c
+++ myshell.c
@@ -8,9 +8,27 @@
 #include <fcntl.h>
 #include "parser.h"
 
+void mycd(char *path, int len) {
+    char *dir;
+	char buffer[512];
+
+    if (len == 0) {
+        dir = getenv("HOME");
+		if (dir == NULL)
+		    fprintf(stderr,"No existe la variable $HOME\n");
+    }
+    else
+        dir = path;
+    
+    if (chdir(dir) != 0)
+	    fprintf(stderr,"Error al cambiar de directorio: %s\n", strerror(errno));
+
+	else
+        printf( "El directorio actual es: %s\n", getcwd(buffer,-1));
+}
+
 int main(){
     char buf[1024];
-    char bufferPipe[1024];
 	tline * line;
 	int i,j;
 
@@ -25,10 +43,18 @@ int main(){
 			continue;
 		}
         
-        if (line->ncommands == 1) {
-            pid = fork();
-            if (pid == 0){
-                if (line->redirect_output != NULL) {
+        else if (line->ncommands == 1) {
+            if (strncmp(buf, "cd", 2) == 0) {
+                int len_buf = strcspn(buf, "\n");
+                char path[len_buf - 2];
+                strncpy(path, &buf[3], len_buf - 3);
+                path[len_buf - 3] = '\0';
+                mycd(path, strlen(path));
+            }
+            else {
+                pid = fork();
+                if (pid == 0) {
+                    if (line->redirect_output != NULL) {
                         int fd_out = open(line->redirect_output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
                         if (fd_out < 0) {
                             printf("Error al abrir el archivo de salida");
@@ -37,7 +63,7 @@ int main(){
                         dup2(fd_out, STDOUT_FILENO);
                         close(fd_out);
                     }
-                if (line->redirect_input != NULL) {
+                    if (line->redirect_input != NULL) {
                         int fd_in = open(line->redirect_input, O_RDONLY);
                         if (fd_in < 0) {
                             printf("Error al abrir el archivo de entrada\n");
@@ -46,12 +72,13 @@ int main(){
                         dup2(fd_in, STDIN_FILENO);
                         close(fd_in);
                     }
-                execvp(line->commands->argv[0], line->commands->argv);
-                fprintf(stderr,"Se ha producido un error.\n");
-                exit(1);
-            }
-            else {
-                wait(NULL);
+                    execvp(line->commands[0].filename, line->commands[0].argv);
+                    fprintf(stderr,"Se ha producido un error.\n");
+                    exit(1);
+                }
+                else {
+                    wait(NULL);
+                }
             }
 
         }
