d643c0883ed8330c38ec35120b8cb2b7e49178a3
dtorresss <diegotorresprofesional@gmail.com>
2024-11-28 10:27:55 +0100

    2,5 puntos conseguidos

--- myshell.c
+++ myshell.c
@@ -10,6 +10,7 @@
 
 int main(){
     char buf[1024];
+    char bufferPipe[1024];
 	tline * line;
 	int i,j;
 
@@ -55,44 +56,49 @@ int main(){
 
         }
 
-        // Aqui hay que crear un hijo y luego otro hijo de el
         else {
-            pipe(pip_des);
             pid = fork();
-            if (pid == 0){
-                /*if (line->redirect_output != NULL) {
-                    int fd_out = open(line->redirect_output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-                    if (fd_out < 0) {
-                        printf("Error al abrir el archivo de salida");
-                        exit(1);
+            if (pid == 0) {
+                pipe(pip_des);
+                pid = fork();
+                if (pid == 0){
+                    if (line->redirect_input != NULL) {
+                        int fd_in = open(line->redirect_input, O_RDONLY);
+                        if (fd_in < 0) {
+                            printf("Error al abrir el archivo de entrada\n");
+                            exit(1);
+                        }
+                        dup2(fd_in, STDIN_FILENO);
+                        close(fd_in);
                     }
-                    dup2(fd_out, STDOUT_FILENO);
-                    close(fd_out);
-                }*/
-                close(pip_des[1]);
-                read(pip_des[0], buf, 1024);
-                close(pip_des[0]);
-                printf("Hijo: Recibido el siguiente mensaje: \"%s\"\n", buf);
-                
-                exit(0);
+                    close(pip_des[0]);
+                    dup2(pip_des[1], STDOUT_FILENO);
+                    close(pip_des[1]);
+                    execvp(line->commands[0].filename, line->commands[0].argv);
+                    fprintf(stderr,"Se ha producido un error.\n");
+                    exit(1);            
+                }
+                else {
+                    if (line->redirect_output != NULL) {
+                        int fd_out = open(line->redirect_output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+                        if (fd_out < 0) {
+                            printf("Error al abrir el archivo de salida");
+                            exit(1);
+                        }
+                        dup2(fd_out, STDOUT_FILENO);
+                        close(fd_out);
+                    }
+                    close(pip_des[1]);
+                    dup2(pip_des[0], STDIN_FILENO);
+                    close(pip_des[0]);
+                    wait(NULL);
+                    execvp(line->commands[1].filename, line->commands[1].argv);
+                    fprintf(stderr,"Se ha producido un error.\n");
+                    exit(1);
+                }
             }
             else {
-                printf("Hola soy el padre\n");
-                /*if (line->redirect_input != NULL) {
-                    int fd_in = open(line->redirect_input, O_RDONLY);
-                    if (fd_in < 0) {
-                        printf("Error al abrir el archivo de entrada\n");
-                        exit(1);
-                    }
-                    dup2(fd_in, STDIN_FILENO);
-                    close(fd_in);
-                } */
-                execvp(line->commands->argv[0], line->commands->argv);
-                close(pip_des[0]);
-                write(pip_des[1], "pwd", 3);
-                close(pip_des[1]);
                 wait(NULL);
-                printf("Padre: El hijo termino");
             }
         }
 		printf("msh> ");	
